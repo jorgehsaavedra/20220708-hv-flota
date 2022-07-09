b2dd538cfe566f8965c412ff9c90f7dc85c3e455
Jorge saavedra <jorge@saavedragomes.com>
2022-07-09 12:10:55 -0500

    Se ajusta el proceso

---
+++ Generar HV.R
@@ -0,0 +1,26 @@
+#**********************************************************************************************
+#  @Nombre: Script para generar la hoja de vida del activo 
+#  @Autor: Jorge Saavedra
+#  @Fecha: 20220709
+#  @Cambios:
+#  @Ayudas:
+#**********************************************************************************************
+
+# Cargue de Librerias -------------------------------------------------------------------------
+library(tidyverse)    # Transformaciones de datos
+library(data.table)   # Transformaciones de datos
+library(lubridate)    # Tratamiento de fechas
+library(httr)         # Publicacion de notificaciones
+options(scipen = 999)
+
+# Cargue de Documentos de referencia ----------------------------------------------------------
+# Para que el scrip funcione debe primero setear en la computadora lo siguiente:
+# @keyring::key_set("DWGM", "TuUsuraio", "TuContraseña")
+
+placa <- "KYO810"
+
+rmarkdown::render(
+  input = "HV Activo.Rmd",  
+  output_file = stringr::str_glue("02 Outputs/{placa}_Report_{format(Sys.Date(), '%Y%m%d')}.html"),
+  params = list(placa = placa, desde = "2022-04-01", hasta = "2022-07-08"))
+
